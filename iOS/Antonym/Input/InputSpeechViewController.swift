c58402fc788ce8bfb45d12d79ef3f98c15fc64ec
midori004 <midori253@gmail.com>
2016-10-29 21:53:44 +0900

    add speech api to info.plist

---
+++ iOS/Antonym/Input/InputSpeechViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InputSpeechViewController.swift
+//  Antonym
+//
+//  Created by 広瀬緑 on 2016/10/29.
+//  Copyright © 2016年 midori hirose. All rights reserved.
+//
+
+import UIKit
+
+class InputSpeechViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
