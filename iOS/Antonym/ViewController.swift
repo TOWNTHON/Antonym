323c72c7427cdde11f322a06cda12ec4e9c3d220
midori004 <midori253@gmail.com>
2016-10-29 10:56:15 +0900

    add initial ios project files

---
+++ iOS/Antonym/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Antonym
+//
+//  Created by 広瀬緑 on 2016/10/29.
+//  Copyright © 2016年 midori hirose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
